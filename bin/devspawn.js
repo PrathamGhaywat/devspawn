afbb3079f211ca7bf2ca3ae15ead1482e90fe29c
PrathamGhaywat <Pratham.Ghaywat@outlook.com>
2025-07-28 14:30:56 +0200

    why am i so fast today? normally this would take me years. i guess reading docs does help. anyways i added the delete functionality to the tool hopefully it works now!

--- bin/devspawn.js
+++ bin/devspawn.js
@@ -3,11 +3,11 @@
 //1st line is a shebang(for system to know that this is an cli thing)
 
 import { program } from "commander";
-import { addShortcut, runShortcut, listShortcuts, editShortcut } from '../lib/commands.js' 
+import { addShortcut, runShortcut, listShortcuts, editShortcut, deleteShortcut } from '../lib/commands.js' 
 
 program.command('add <name> [cmds...]').description('Add a new command chain').action(addShortcut)
 program.command('run <name> [arg]').description('Run a shortcut by name').action(runShortcut)
 program.command('list').description('List all saved shortcuts').action(listShortcuts)
 program.command('edit <name> [cmds...').description('Edit a saved shortcut').action(editShortcut)
-program.command('delete <name> ').description('Delete a shortcut').action(editShortcut)
+program.command('delete <name> ').description('Delete a shortcut').action(deleteShortcut)
 program.parse(process.argv)
