e20c1947f3190ecd9dde92944a4b7b655d6b5232
PrathamGhaywat <Pratham.Ghaywat@outlook.com>
2025-07-28 14:18:23 +0200

    added edit functionality

--- bin/devspawn.js
+++ bin/devspawn.js
@@ -3,10 +3,11 @@
 //1st line is a shebang(for system to know that this is an cli thing)
 
 import { program } from "commander";
-import { addShortcut, runShortcut, listShortcuts } from '../lib/commands.js' //TODO: Add this feature
+import { addShortcut, runShortcut, listShortcuts, editShortcut } from '../lib/commands.js' 
 
 program.command('add <name> [cmds...]').description('Add a new command chain').action(addShortcut)
 program.command('run <name> [arg]').description('Run a shortcut by name').action(runShortcut)
 program.command('list').description('List all saved shortcuts').action(listShortcuts)
+program.command('edit <name> [cmds...').description('Edit an saved shortcut').action(editShortcut)
 
 program.parse(process.argv)
