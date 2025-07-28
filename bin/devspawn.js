1749ea4feb3997d007338edb340b9a96c4d9039e
PrathamGhaywat <Pratham.Ghaywat@outlook.com>
2025-07-28 14:23:33 +0200

    can't believe i made an typo :-)

--- bin/devspawn.js
+++ bin/devspawn.js
@@ -8,6 +8,6 @@ import { addShortcut, runShortcut, listShortcuts, editShortcut } from '../lib/co
 program.command('add <name> [cmds...]').description('Add a new command chain').action(addShortcut)
 program.command('run <name> [arg]').description('Run a shortcut by name').action(runShortcut)
 program.command('list').description('List all saved shortcuts').action(listShortcuts)
-program.command('edit <name> [cmds...').description('Edit an saved shortcut').action(editShortcut)
-
+program.command('edit <name> [cmds...').description('Edit a saved shortcut').action(editShortcut)
+program.command('delete <name> ').description('Delete a shortcut').action(editShortcut)
 program.parse(process.argv)
